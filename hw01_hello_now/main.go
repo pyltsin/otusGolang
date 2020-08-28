ef05ca374c5a9f05ed0aaa54541cda69e4402a7c
pyltsin <pyltsin-m@yandex.ru>
2020-08-28 18:15:48 +0300

    hw1

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+
+	now := time.Now()
+	ntpNow, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		log.Fatalf("Error getting time: %s", err)
+	}
+
+	fmt.Printf("current time: %s\n", now.Round(0))
+	fmt.Printf("exact time: %s\n", ntpNow.Round(0))
 }
