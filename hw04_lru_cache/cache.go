ce957c4b6fffd56a4cf4fb4a310fcd6acc542148
pyltsin <pyltsin-m@yandex.ru>
2020-09-17 16:58:35 +0300

    hw04_lru_cache

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,22 +1,85 @@
 package hw04_lru_cache //nolint:golint,stylecheck
+import "sync"
 
 type Key string
 
 type Cache interface {
-	// Place your code here
-}
-
-type lruCache struct {
-	// Place your code here:
-	// - capacity
-	// - queue
-	// - items
+	Set(key Key, value interface{}) bool // Добавить значение в кэш по ключу
+	Get(key Key) (interface{}, bool)     // Получить значение из кэша по ключу
+	Clear()                              // Очистить кэш
 }
 
 type cacheItem struct {
-	// Place your code here
+	key   Key
+	value interface{}
+}
+
+type lruCache struct {
+	items    map[Key]*ListItem
+	queue    List
+	capacity int
+	mutex    sync.Mutex
+}
+
+func (l *lruCache) Set(key Key, value interface{}) bool {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+	item, ok := l.items[key]
+	if ok {
+		cachedItem := getAsCachedItem(item)
+		cachedItem.value = value
+	} else {
+		queueItem := l.queue.PushFront(&cacheItem{
+			key:   key,
+			value: value,
+		})
+		if l.queue.Len() > l.capacity {
+			removedQueueItem := l.queue.Back()
+			l.queue.Remove(removedQueueItem)
+
+			delete(l.items, getAsCachedItem(removedQueueItem).key)
+		}
+
+		l.items[key] = queueItem
+	}
+
+	return ok
+}
+
+func getAsCachedItem(item *ListItem) *cacheItem {
+	cachedItem, ok := item.Value.(*cacheItem)
+	if !ok {
+		panic("not expected item")
+	}
+	return cachedItem
+}
+
+func (l *lruCache) Get(key Key) (interface{}, bool) {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+
+	item, ok := l.items[key]
+	if ok {
+		cachedItem := getAsCachedItem(item)
+		l.queue.MoveToFront(item)
+		return cachedItem.value, true
+	} else {
+		return nil, false
+	}
+}
+
+func (l *lruCache) Clear() {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+
+	l.items = make(map[Key]*ListItem)
+	l.queue = NewList()
 }
 
 func NewCache(capacity int) Cache {
-	return &lruCache{}
+	return &lruCache{
+		capacity: capacity,
+		queue:    NewList(),
+		items:    make(map[Key]*ListItem),
+	}
 }
--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -15,6 +15,181 @@ func TestList(t *testing.T) {
 		require.Nil(t, l.Back())
 	})
 
+	t.Run("len change only when len list change", func(t *testing.T) {
+		l := NewList()
+
+		require.Equal(t, 0, l.Len())
+
+		require.Nil(t, l.Front())
+		require.Nil(t, l.Back())
+		require.Equal(t, 0, l.Len())
+
+		l.PushFront(1)
+		require.Equal(t, 1, l.Len())
+
+		movedItem := l.PushBack(2)
+		require.Equal(t, 2, l.Len())
+
+		l.MoveToFront(movedItem)
+		require.Equal(t, 2, l.Len())
+
+		l.Remove(movedItem)
+		require.Equal(t, 1, l.Len())
+	})
+
+	t.Run("add to front", func(t *testing.T) {
+		l := NewList()
+
+		l.PushFront(3)
+		l.PushFront(2)
+		l.PushFront(1)
+
+		require.Equal(t, 1, l.Front().Value)
+		require.Equal(t, 2, l.Front().Next.Value)
+		require.Equal(t, 3, l.Front().Next.Next.Value)
+		require.Nil(t, nil, l.Front().Next.Next.Next)
+
+		require.Equal(t, 3, l.Back().Value)
+		require.Equal(t, 2, l.Back().Prev.Value)
+		require.Equal(t, 1, l.Back().Prev.Prev.Value)
+		require.Nil(t, nil, l.Back().Prev.Prev.Prev)
+	})
+
+	t.Run("add to back", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(1)
+		l.PushBack(2)
+		l.PushBack(3)
+
+		require.Equal(t, 1, l.Front().Value)
+		require.Equal(t, 2, l.Front().Next.Value)
+		require.Equal(t, 3, l.Front().Next.Next.Value)
+		require.Nil(t, nil, l.Front().Next.Next.Next)
+
+		require.Equal(t, 3, l.Back().Value)
+		require.Equal(t, 2, l.Back().Prev.Value)
+		require.Equal(t, 1, l.Back().Prev.Prev.Value)
+		require.Nil(t, nil, l.Back().Prev.Prev.Prev)
+	})
+
+	//move to front from middle
+	//move to front from front
+
+	t.Run("move to front from back", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(1)
+		l.PushBack(2)
+		back := l.PushBack(3)
+
+		l.MoveToFront(back)
+
+		require.Equal(t, 3, l.Front().Value)
+		require.Equal(t, 1, l.Front().Next.Value)
+		require.Equal(t, 2, l.Front().Next.Next.Value)
+		require.Nil(t, nil, l.Front().Next.Next.Next)
+
+		require.Equal(t, 2, l.Back().Value)
+		require.Equal(t, 1, l.Back().Prev.Value)
+		require.Equal(t, 3, l.Back().Prev.Prev.Value)
+		require.Nil(t, nil, l.Back().Prev.Prev.Prev)
+	})
+
+	t.Run("move to front from front", func(t *testing.T) {
+		l := NewList()
+
+		front := l.PushBack(1)
+		l.PushBack(2)
+		l.PushBack(3)
+
+		l.MoveToFront(front)
+
+		require.Equal(t, 1, l.Front().Value)
+		require.Equal(t, 2, l.Front().Next.Value)
+		require.Equal(t, 3, l.Front().Next.Next.Value)
+		require.Nil(t, nil, l.Front().Next.Next.Next)
+
+		require.Equal(t, 3, l.Back().Value)
+		require.Equal(t, 2, l.Back().Prev.Value)
+		require.Equal(t, 1, l.Back().Prev.Prev.Value)
+		require.Nil(t, nil, l.Back().Prev.Prev.Prev)
+	})
+
+	t.Run("move to front from middle", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(1)
+		middle := l.PushBack(2)
+		l.PushBack(3)
+
+		l.MoveToFront(middle)
+
+		require.Equal(t, 2, l.Front().Value)
+		require.Equal(t, 1, l.Front().Next.Value)
+		require.Equal(t, 3, l.Front().Next.Next.Value)
+		require.Nil(t, nil, l.Front().Next.Next.Next)
+
+		require.Equal(t, 3, l.Back().Value)
+		require.Equal(t, 1, l.Back().Prev.Value)
+		require.Equal(t, 2, l.Back().Prev.Prev.Value)
+		require.Nil(t, nil, l.Back().Prev.Prev.Prev)
+	})
+
+	t.Run("remove from front", func(t *testing.T) {
+		l := NewList()
+
+		front := l.PushBack(1)
+		l.PushBack(2)
+		l.PushBack(3)
+
+		l.Remove(front)
+
+		require.Equal(t, 2, l.Front().Value)
+		require.Equal(t, 3, l.Front().Next.Value)
+		require.Nil(t, nil, l.Front().Next.Next)
+
+		require.Equal(t, 3, l.Back().Value)
+		require.Equal(t, 2, l.Back().Prev.Value)
+		require.Nil(t, nil, l.Back().Prev.Prev)
+	})
+
+	t.Run("remove from back", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(1)
+		l.PushBack(2)
+		back := l.PushBack(3)
+
+		l.Remove(back)
+
+		require.Equal(t, 1, l.Front().Value)
+		require.Equal(t, 2, l.Front().Next.Value)
+		require.Nil(t, nil, l.Front().Next.Next)
+
+		require.Equal(t, 2, l.Back().Value)
+		require.Equal(t, 1, l.Back().Prev.Value)
+		require.Nil(t, nil, l.Back().Prev.Prev)
+	})
+
+	t.Run("remove from middle", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(1)
+		middle := l.PushBack(2)
+		l.PushBack(3)
+
+		l.Remove(middle)
+
+		require.Equal(t, 1, l.Front().Value)
+		require.Equal(t, 3, l.Front().Next.Value)
+		require.Nil(t, nil, l.Front().Next.Next)
+
+		require.Equal(t, 3, l.Back().Value)
+		require.Equal(t, 1, l.Back().Prev.Value)
+		require.Nil(t, nil, l.Back().Prev.Prev)
+	})
+
 	t.Run("complex", func(t *testing.T) {
 		l := NewList()
 
