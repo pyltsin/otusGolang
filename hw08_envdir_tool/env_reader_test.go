c39ceb70d23de0b63f79129a9e778a2e5bf912ce
pyltsin-m <pyltsin-m@yandex.ru>
2020-11-06 21:53:37 +0300

    8

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,50 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	baseDir := "testdata/env"
+	countFiles := 4
+
+	t.Run("read env vars", func(t *testing.T) {
+		envs, err := ReadDir(baseDir)
+		assert.Equal(t, nil, err)
+		assert.Equal(t, countFiles, len(envs))
+	})
+
+	t.Run("skip file with =", func(t *testing.T) {
+		invalidFileName := "test="
+		tmpFile, _ := ioutil.TempFile(baseDir, invalidFileName)
+		defer os.Remove(tmpFile.Name())
+		envs, err := ReadDir(baseDir)
+		assert.Equal(t, nil, err)
+		assert.Equal(t, countFiles, len(envs))
+	})
+
+	t.Run("collect empty env string from empty file", func(t *testing.T) {
+		fileName := "test"
+		tmpFile, _ := ioutil.TempFile(baseDir, fileName)
+		defer os.Remove(tmpFile.Name())
+		envs, err := ReadDir(baseDir)
+		assert.Equal(t, nil, err)
+		assert.Equal(t, countFiles+1, len(envs))
+		assert.Equal(t, "", envs[filepath.Base(tmpFile.Name())])
+	})
+
+	t.Run("remove whitespace characters from env var", func(t *testing.T) {
+		fileName := "test"
+		tmpFile, _ := ioutil.TempFile(baseDir, fileName)
+		defer os.Remove(tmpFile.Name())
+		_ = ioutil.WriteFile(tmpFile.Name(), []byte("first line\t \t \nsecond line"), os.ModePerm)
+		envs, err := ReadDir(baseDir)
+		assert.Equal(t, nil, err)
+		assert.Equal(t, countFiles+1, len(envs))
+		assert.Equal(t, "first line", envs[filepath.Base(tmpFile.Name())])
+	})
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,37 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	t.Run("success run cmd", func(t *testing.T) {
+		tmpCmd := []string{"echo"}
+		code := RunCmd(tmpCmd, nil)
+		assert.Equal(t, successCode, code)
+	})
+	t.Run("success set env variable", func(t *testing.T) {
+		tmpCmd := []string{"echo"}
+		tmpEnv := Environment{"TEST": "test"}
+		code := RunCmd(tmpCmd, tmpEnv)
+		assert.Equal(t, successCode, code)
+		assert.Equal(t, "test", os.ExpandEnv("$TEST"))
+	})
+	t.Run("return code from command", func(t *testing.T) {
+		tmpCmd := []string{"/bin/bash", "wrong_name"}
+		code := RunCmd(tmpCmd, nil)
+		assert.Equal(t, 127, code)
+	})
+	t.Run("unset env var", func(t *testing.T) {
+		os.Setenv("TEST", "test")
+		tmpCmd := []string{"echo"}
+		tmpEnv := Environment{"TEST": ""}
+		code := RunCmd(tmpCmd, tmpEnv)
+		assert.Equal(t, successCode, code)
+		remEnv, isPresent := os.LookupEnv("$TEST")
+		assert.Equal(t, false, isPresent)
+		assert.Equal(t, "", remEnv)
+	})
 }
