9c07aed68f73c09b377c94c65347df561cee8461
pyltsin-m <pyltsin-m@yandex.ru>
2020-11-21 00:05:33 +0300

    hw10_program_optimization

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,14 +1,15 @@
 package hw10_program_optimization //nolint:golint,stylecheck
 
 import (
+	"bufio"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
-	"io/ioutil"
-	"regexp"
 	"strings"
 )
 
+//easyjson -all stats.go
 type User struct {
 	ID       int
 	Name     string
@@ -22,46 +23,70 @@ type User struct {
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	u, err := getUsers(r)
-	if err != nil {
-		return nil, fmt.Errorf("get users error: %s", err)
-	}
-	return countDomains(u, domain)
-}
-
-type users [100_000]User
-
-func getUsers(r io.Reader) (result users, err error) {
-	content, err := ioutil.ReadAll(r)
-	if err != nil {
-		return
-	}
-
-	lines := strings.Split(string(content), "\n")
-	for i, line := range lines {
-		var user User
-		if err = json.Unmarshal([]byte(line), &user); err != nil {
-			return
-		}
-		result[i] = user
-	}
-	return
-}
-
-func countDomains(u users, domain string) (DomainStat, error) {
 	result := make(DomainStat)
+	rd := bufio.NewReader(r)
+	user := User{}
+	pattern := "." + domain
+	var finish bool
+	for !finish {
+		bytes, err := rd.ReadBytes('\n')
 
-	for _, user := range u {
-		matched, err := regexp.Match("\\."+domain, []byte(user.Email))
+		if err != nil && !errors.Is(err, io.EOF) {
+			return nil, err
+		}
+
+		if errors.Is(err, io.EOF) {
+			finish = true
+		}
+
+		err = unmarshal(bytes, &user)
 		if err != nil {
 			return nil, err
 		}
 
-		if matched {
-			num := result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
-			num++
-			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])] = num
+		if user.Email == "" {
+			return nil, fmt.Errorf("not found email")
+		}
+
+		domain, err = getDomain(&user, pattern)
+
+		if err != nil {
+			return nil, err
+		}
+
+		if domain == "" {
+			continue
+		}
+
+		i, ok := result[domain]
+		if !ok {
+			result[domain] = 1
+		} else {
+			result[domain] = i + 1
 		}
 	}
+
 	return result, nil
+}
+
+func getDomain(user *User, pattern string) (string, error) {
+	containsPoint := strings.Contains(user.Email, ".")
+	if !containsPoint {
+		return "", fmt.Errorf("not contain '.'")
+	}
+
+	hasSuffix := strings.HasSuffix(user.Email, pattern)
+	if !hasSuffix {
+		return "", nil
+	}
+
+	splitted := strings.Split(user.Email, "@")
+	if len(splitted) != 2 {
+		return "", fmt.Errorf("not contain @")
+	}
+	return strings.ToLower(splitted[1]), nil
+}
+
+func unmarshal(line []byte, u json.Unmarshaler) error {
+	return u.UnmarshalJSON(line)
 }
